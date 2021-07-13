7b3f3d710c012f6fbcc948ccd1447cc9fe98418c
sindhuvahini <Revathi@1994>
2021-07-13 23:38:44 +0530

    changes in preprocessing file

--- resources/pre_processing.py
+++ resources/pre_processing.py
@@ -6,7 +6,7 @@ class Preprocessor(object):
     def initialize(self):
         pass
 
-    def preprocess(self):
+    def preprocess(self, input):
         int_data = np.arange(9).reshape(3, 3)
         float_data = np.arange(9.0).reshape(3, 3)
         return [int_data, float_data]
